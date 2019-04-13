d8ba5b98175e8a2f18e62990cb9b4a6c570bc239
zadaya <zhadaiyan@163.com>
2019-04-14 01:59:34 +0800

    修改zhuabao.py功能，使其筛选得到DNS应答报文

--- zhuabao.py
+++ zhuabao.py
@@ -4,12 +4,21 @@ from scapy.layers.inet import *
 from scapy.all import sniff
 
 def pack_callback(packet):
-    print(packet.show())
+    # print(packet.show())
     if packet[UDP].payload:
-        mail_packet = str(packet[UDP].payload)
-        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
-            print("Server:%s" % packet[IP].dst)
-            print("%s" % packet[UDP].payload)
+        if (packet.sprintf("%DNS.qr%") == "1"):
+            print(packet.show())
+        # if (packet.sprintf("%DNS.qr%") == "0"):
+        #     print("\n\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n\n")
+
+        # aaaa=packet.sprintf("Etherent source: %Ether.src%    IP src: %IP.src%    DNS src: %DNS.id%")
+        # print(aaaa)
+        # print("###################################################################")
+    
+        # DNS_packet = str(packet[UDP].payload)
+        # if "user" in DNS_packet.lower() or "pass" in DNS_packet.lower():
+        #     print("Server:%s" % packet[IP].dst)
+        #     print("%s" % packet[UDP].payload)
 
 #sniff()第一个参数可以筛选协议类型及端口号，第二个参数设置监听的网卡名
-sniff(filter="udp port 53", iface='Realtek PCIe GbE Family Controller', prn=pack_callback, count=100)
+dnsSniffPacket = sniff(filter="udp port 53", iface='Realtek PCIe GbE Family Controller', prn=pack_callback, count=100)
