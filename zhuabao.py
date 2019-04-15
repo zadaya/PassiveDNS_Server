fd1e622d2e96791ffa2374e29b3cab8e1ad2ffbb
王冠 <wangguan@wangguandeMac-mini.local>
2019-04-16 00:41:32 +0800

    iface 改为：默认从环境变量获取

--- zhuabao.py
+++ zhuabao.py
@@ -1,27 +1,35 @@
 from scapy.layers.inet import *
 from scapy.all import sniff
+import os
+
 
 def pack_callback(packet):
     # print(packet.show())
     if packet[UDP].payload:
-        if (packet.sprintf("%DNS.qr%") == "1"):
+        if packet.sprintf("%DNS.qr%") == "1":
             print(packet[UDP].show())
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print(
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            )
             domain_str = packet.sprintf("%DNSQR.qname%")
             ipaddr_str = packet.sprintf("%DNSRR.rdata%")
-            print(domain_str+":"+ipaddr_str)
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print(domain_str + ":" + ipaddr_str)
+            print(
+                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            )
         # if (packet.sprintf("%DNS.qr%") == "0"):
         #     print("\n\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n\n")
 
         # aaaa=packet.sprintf("Etherent source: %Ether.src%    IP src: %IP.src%    DNS src: %DNS.id%")
         # print(aaaa)
         # print("###################################################################")
-    
+
         # DNS_packet = str(packet[UDP].payload)
         # if "user" in DNS_packet.lower() or "pass" in DNS_packet.lower():
         #     print("Server:%s" % packet[IP].dst)
         #     print("%s" % packet[UDP].payload)
 
-#sniff()第一个参数可以筛选协议类型及端口号，第二个参数设置监听的网卡名
-dnsSniffPacket = sniff(filter="udp port 53", iface='Realtek PCIe GbE Family Controller', prn=pack_callback, count=100)
+
+# sniff()第一个参数可以筛选协议类型及端口号，第二个参数设置监听的网卡名
+iface = os.getenv("IFACE", "Realtek PCIe GbE Family Controller")
+dnsSniffPacket = sniff(filter="udp port 53", iface=iface, prn=pack_callback, count=100)
